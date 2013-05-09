e96601eb298eeb19c728dbb56d0bbf36950c7e8d
Flávio J. Saraiva <flaviojs2005@gmail.com>
2013-05-09 21:09:41 +0100

    Search children of symbols from the tree (separate thread). Rework the content of the welcome page. Rework some code.

--- pydiagui.py
+++ pydiagui.py
@@ -1,6 +1,6 @@
 
 
-import sys, os, traceback
+import sys, os, traceback, thread
 import wx
 import wx.py.crust
 import wx.html
@@ -22,27 +22,51 @@ class Log:
 
 #---------------------------------------------------------------------------
 
+class TreeData(object):
 
-class SymbolPanel(wx.Panel):
-    def __init__(self, parent, session, symbol, log):
-        wx.Panel.__init__(self, parent)
-
-        assert session
-        assert symbol
-        self.log = Log()
-        self.symIndexId = symbol.symIndexId
-        
-        self.label = wx.StaticText(self, -1, label = "Metadata:")
-        metadata = pydia.SymbolPrinter(session).metadata(symbol)
-        self.text = wx.TextCtrl(self, -1, value = '\n'.join(metadata), style = wx.TE_MULTILINE | wx.TE_READONLY)
-
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.label, 0)
-        sizer.Add(self.text, 0, wx.EXPAND)
-        self.SetSizer(sizer)
+    SEARCH = "search"
+    SYMBOL = "symbol"
+    
+    def GetTreeDataType(self):
+        raise NotImplementedError("TreeData.GetTreeDataType")
 
 
-class SymbolData(object):
+class FindChildrenData(TreeData):
+    def __init__(self):
+        self.name = ""
+        self.type = pydia.SYMTAG.SymTagNull
+        self.flags = pydia.NameSearchOptions.nsNone
+        self.busy = False
+
+    def GetTreeDataType(self):
+        return TreeData.SEARCH
+
+    def GetTitle(self):
+        return "Find children... [{}]".format(",".join(self.GetOptions()))
+
+    def GetOptions(self):
+        opt = []
+        if self.name or self.type or self.flags:
+            opt.append(self.name)
+            if self.type:
+                opt.append(pydia.SYMTAG_name(self.type))
+            for k in pydia.NameSearchOptions.__dict__.keys():
+                if k.startswith("ns") and (self.flags & getattr(pydia.NameSearchOptions, k)):
+                    opt.append(k)
+        return opt
+
+    def GetNewOptions(self, parent):
+        dlg = FindChildrenDialog(self, parent = parent, title = "Find children")
+        result = dlg.ShowModal()
+        if result == wx.ID_OK:
+            self.name = dlg.GetName()
+            self.type = dlg.GetType()
+            self.flags = dlg.GetFlags()
+        dlg.Destroy()
+        return result
+
+
+class SymbolData(TreeData):
     def __init__(self, session, symbol):
         assert session
         assert symbol
@@ -52,6 +76,133 @@ class SymbolData(object):
     def __del__(self):
         del self.symbol
         del self.session
+
+    def GetTreeDataType(self):
+        return TreeData.SYMBOL
+
+    def GetTitle(self, text = None, attribute = False):
+        symIndexId = self.symbol.symIndexId
+        if text:
+            return "{} - {}".format(symIndexId, text)
+        symTagName = pydia.SYMTAG_name(self.symbol.symTag)
+        name = self.symbol.name
+        if attribute:
+            return "{} - {} = {} , {}".format(symIndexId, attribute, symTagName, name)
+        return "{} - {} , {}".format(symIndexId, symTagName, name)
+
+#---------------------------------------------------------------------------
+
+class FindChildrenDialog(wx.Dialog):
+    def __init__(self, data, *args, **kwargs):
+        wx.Dialog.__init__(self, *args, **kwargs)
+
+        try:
+            sizer = wx.BoxSizer(wx.VERTICAL)
+
+            # name
+            box = wx.BoxSizer(wx.HORIZONTAL)
+
+            label = wx.StaticText(self, wx.ID_ANY, "Name :")
+            box.Add(label, 0, wx.RIGHT, 5)
+
+            self.name = wx.TextCtrl(self, wx.ID_ANY, "")
+            self.name.SetValue(data.name)
+            box.Add(self.name, 1, wx.EXPAND)
+
+            sizer.Add(box, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
+
+            # type
+            value = pydia.SYMTAG_name(data.type)
+            choices = [k for k in pydia.SYMTAG.__dict__.keys() if k.startswith("SymTag")]
+            assert value in choices
+            
+            box = wx.BoxSizer(wx.HORIZONTAL)
+
+            label = wx.StaticText(self, wx.ID_ANY, "Type :")
+            box.Add(label, 0, wx.RIGHT, 5)
+
+            self.type = wx.ComboBox(self, value = value, choices = choices, style = wx.CB_READONLY)
+            self.type.SetValue(value)
+            box.Add(self.type, 1, wx.EXPAND)
+
+            sizer.Add(box, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.LEFT | wx.RIGHT, 5)
+
+            # flags
+            static = wx.StaticBox(self, wx.ID_ANY, label = "Flags :")
+
+            box = wx.StaticBoxSizer(static, wx.VERTICAL)
+
+            self.flags = dict()
+            for flagname in sorted([k for k in pydia.NameSearchOptions.__dict__.keys() if k.startswith("ns")]):
+                flag = getattr(pydia.NameSearchOptions, flagname)
+                if flag:
+                    check = wx.CheckBox(self, label = flagname, style = wx.CHK_2STATE)
+                    if (flag & data.flags):
+                        check.SetValue(True)
+                    box.Add(check, 0, wx.ALIGN_LEFT)
+                    self.flags[flagname] = check
+
+            sizer.Add(box, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.LEFT | wx.RIGHT, 5)
+
+            # buttons
+            btnsizer = wx.StdDialogButtonSizer()
+            
+            btn = wx.Button(self, wx.ID_OK)
+            btn.SetDefault()
+            btnsizer.AddButton(btn)
+
+            btn = wx.Button(self, wx.ID_CANCEL)
+            btnsizer.AddButton(btn)
+            btnsizer.Realize()
+
+            sizer.Add(btnsizer, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
+
+            self.SetSizer(sizer)
+            sizer.Fit(self)
+        except:
+            traceback.print_exc()
+            self.Destroy()
+
+    def GetName(self):
+        return self.name.GetValue()
+
+    def GetType(self):
+        return getattr(pydia.SYMTAG, self.type.GetValue())
+
+    def GetFlags(self):
+        flags = 0
+        for flagname in self.flags.keys():
+            if self.flags[flagname].GetValue():
+                flags = flags | getattr(pydia.NameSearchOptions, flagname)
+        return flags
+
+
+class SymbolPanel(wx.Panel):
+    def __init__(self, parent, session, symbol, log):
+        wx.Panel.__init__(self, parent)
+        try:
+            assert session
+            assert symbol
+            self.log = Log()
+            self.symIndexId = symbol.symIndexId
+
+            sizer = wx.BoxSizer(wx.VERTICAL)
+
+            # flags
+            static = wx.StaticBox(self, label = "Metadata :")
+
+            box = wx.StaticBoxSizer(static, wx.VERTICAL)
+
+            metadata = pydia.SymbolPrinter(session).metadata(symbol)
+            self.text = wx.TextCtrl(self, -1, value = '\n'.join(metadata), style = wx.TE_MULTILINE | wx.TE_READONLY)
+            box.Add(self.text, 0, wx.EXPAND)
+            
+            sizer.Add(box, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
+            
+            self.SetSizer(sizer)
+            sizer.Fit(self)
+        except:
+            traceback.print_exc()
 
 
 class PyDiaGUI(wx.Frame):
@@ -86,23 +237,16 @@ class PyDiaGUI(wx.Frame):
         self.statusbar = self.CreateStatusBar()
 
         try:
-            #print "menubar"
             self.MakeMenuBar()
-            #print "toolbar"
             self.MakeToolBar()
-            #print "shell"
             self.MakeShellPane()
-            #print "symboltree"
             self.MakeSymbolTreePane()
-            #print "symbolbook"
             self.MakeSymbolBook()
-            #print "events"
             self.BindEvents()
 
             self.AddWelcomePage()
             self.statusbar.SetStatusText("Ready")
             self.DoUpdate()
-            #print "done"
         except:
             self.ShowExceptionInDialog()
 
@@ -170,41 +314,26 @@ class PyDiaGUI(wx.Frame):
     def OnTreeItemExpanding(self, event):
         tree = self.mgr.GetPane("symboltree").window
         item = event.GetItem()
-        if item and item.IsOk() and tree.ItemHasChildren(item) and tree.GetChildrenCount(item, False) == 0:
-            # attribute symbols
-            data = tree.GetPyData(item)
-            for attr in pydia.SymbolPrinter(data.session).attributes(data.symbol):
-                try:
-                    symbol = getattr(data.symbol, attr)
-                    if symbol and type(symbol) == type(data.symbol):
-                        title = "{} - {} = {} {}".format(
-                            symbol.symIndexId, attr,
-                            pydia.SYMTAG_name(symbol.symTag), symbol.name)
-                        image = self.TREE_ART_ATTRIBUTE_SYMBOL
-                        data = SymbolData(data.session, symbol)
-                        child = tree.AppendItem(item, title, image)
-                        tree.SetPyData(child, data)
-                        tree.SetItemHasChildren(child, True)
-                except:
-                    pass
-            # search filter
-            title = "Find children..."
-            image = self.TREE_ART_SEARCH_FILTER
-            tree.AppendItem(item, title, image)
-            #print "OnTreeItemExpanding: children added"
+        assert item.IsOk()
+        data = tree.GetPyData(item)
+        if tree.ItemHasChildren(item) and tree.GetChildrenCount(item, False) == 0:
+            if data.GetTreeDataType() == TreeData.SEARCH:
+                self.SearchForChildren(item)
+            elif data.GetTreeDataType() == TreeData.SYMBOL:
+                self.FillSymbolTreeItem(item)
 
     def OnTreeItemActivate(self, event):
         tree = self.mgr.GetPane("symboltree").window
         item = event.GetItem()
-        if item and item.IsOk():
-            image = tree.GetItemImage(item)
-            data = tree.GetPyData(item)
-            if image == self.TREE_ART_SEARCH_FILTER:
-                # search for children
-                print "TODO search filter"
-            elif data: # symbol
-                self.AddSymbolPage(data, tree.GetImageList().GetBitmap(image))
-                self.DoUpdate()
+        assert item.IsOk()
+        image = tree.GetItemImage(item)
+        data = tree.GetPyData(item)
+        if data.GetTreeDataType() == TreeData.SEARCH:
+            # search for children
+            self.SearchForChildren(item)
+        elif data: # symbol
+            self.AddSymbolPage(data, tree.GetImageList().GetBitmap(image))
+            self.DoUpdate()
 
     def DoUpdate(self):
         self.mgr.Update()
@@ -299,12 +428,12 @@ class PyDiaGUI(wx.Frame):
 
         tree = self.mgr.GetPane("symboltree").Show().window
         symbol = self.session.globalScope
-        title = "{} - {}".format(symbol.symIndexId, path)
         image = self.TREE_ART_ROOT_SYMBOL
         data = SymbolData(session, symbol)
-        root = tree.AddRoot(title, image)
+        root = tree.AddRoot(data.GetTitle(text = path), image)
         tree.SetPyData(root, data)
-        tree.SetItemHasChildren(root, True)
+        self.FillSymbolTreeItem(root)
+        tree.Expand(root)
 
         self.GetMenuBar().FindItemById(wx.ID_CLOSE).Enable(True)
         self.AddSymbolPage(data, tree.GetImageList().GetBitmap(image))
@@ -330,15 +459,113 @@ class PyDiaGUI(wx.Frame):
         dlg.ShowModal()
         dlg.Destroy()
 
+    def SearchForChildren(self, item):
+        assert item.IsOk()
+        tree = self.mgr.GetPane("symboltree").window
+        data = tree.GetPyData(item)
+        assert data.GetTreeDataType() == TreeData.SEARCH
+        if not data.busy and data.GetNewOptions(self) == wx.ID_OK:
+            parent = tree.GetItemParent(item)
+            assert parent.IsOk()
+            symdata = tree.GetPyData(parent)
+            assert symdata.GetTreeDataType() == TreeData.SYMBOL
+
+            data.busy = True
+            tree.SetItemText(item, "...(searching)... [{}]".format(','.join(data.GetOptions())))
+            thread.start_new_thread(self._FindChildrenOfSymbol, (item, data, symdata))
+
+    def _FindChildrenOfSymbol(self, item, data, symdata):
+        """runs on worker thread"""
+        assert item.IsOk()
+        assert data.GetTreeDataType() == TreeData.SEARCH
+        assert symdata.GetTreeDataType() == TreeData.SYMBOL
+
+        name = data.name or None
+        type = data.type
+        flags = data.flags
+        children = None
+        try:
+            # NOTE this can take a long time...
+            children = list(symdata.session.findChildrenEx(symdata.symbol, type, name, flags))
+        except:
+            traceback.print_exc()
+            children = []
+        wx.CallAfter(self.AddChildrenToTree, item, symdata.session, children)
+
+    def AddChildrenToTree(self, item, session, children):
+        assert item.IsOk()
+        tree = self.mgr.GetPane("symboltree").window
+        tree.DeleteChildren(item)
+        for child in children:
+            data = SymbolData(session, child)
+            childitem = tree.AppendItem(item, data.GetTitle(), self.TREE_ART_CHILD_SYMBOL)
+            assert childitem.IsOk()
+            tree.SetPyData(childitem, data)
+            tree.SetItemHasChildren(childitem)
+        data = tree.GetPyData(item)
+        tree.SetItemText(item, data.GetTitle())
+        tree.SetItemHasChildren(item, len(children) > 0)
+        tree.Expand(item)
+        tree.GetPyData(item).busy = False
+        self.DoUpdate()
+
+    def FillSymbolTreeItem(self, item):
+        tree = self.mgr.GetPane("symboltree").window
+        assert item
+        assert item.IsOk()
+        data = tree.GetPyData(item)
+
+        # attribute symbols
+        for attribute in pydia.SymbolPrinter(data.session).attributes(data.symbol):
+            try:
+                symbol = getattr(data.symbol, attribute)
+                if symbol and type(symbol) == type(data.symbol):
+                    image = self.TREE_ART_ATTRIBUTE_SYMBOL
+                    data = SymbolData(data.session, symbol)
+                    childitem = tree.AppendItem(item, data.GetTitle(attribute = attribute), image)
+                    tree.SetPyData(childitem, data)
+                    tree.SetItemHasChildren(childitem, True)
+            except:
+                pass
+
+        # search filter
+        image = self.TREE_ART_SEARCH_FILTER
+        data = FindChildrenData()
+        finditem = tree.AppendItem(item, data.GetTitle(), image)
+        tree.SetPyData(finditem, data)
+        tree.SetItemHasChildren(finditem, True)
+
+#---------------------------------------------------------------------------
 
 def GetIntroText():
     text = \
     "<html><body>" \
     "<h3>Welcome to PyDiaGUI</h3>" \
-    "<p>PyDiaGUI is a graphical user interface to PyDia, a collection of classes to explore debug symbols from MSDIA in python.<p>" \
-    "<p>MSDIA is the Microsoft Debug Interface Access Software Development Kit (DIA SDK) that comes with Visual Studio.<p>" \
+    "<p>Sections:</p>" \
+    "<ol>" \
+    "<li><a href='#overview'>Overview</a></li>" \
+    "<li><a href='#instructions'>Instructions</a></li>" \
+    "<li><a href='#feedback'>Feedback</a></li>" \
+    "</ol><hr />" \
+    "<a name='overview' /><h4>Overview</h4>" \
+    "<p>PyDiaGUI is a graphical user interface to PyDia, a collection of classes to explore debug symbols from MSDIA in python. " \
+    "MSDIA is the Microsoft Debug Interface Access Software Development Kit (DIA SDK) that comes with Visual Studio.</p>" \
     "<ul><li>http://msdn.microsoft.com/en-us/library/x93ctkx8.aspx</li></ul>" \
-    "<p>Please report any bugs or requests of improvements to:<p>" \
+    "<a name='instructions' /><h4>Instructions</h4>" \
+    "<p>Open a file that contains compatible debug symbols, usually a <code>.pdb</code> file, with <code>File &rarr; Open...</code>. " \
+    "A tree control will appear at the bottom of the window with the root debug symbol of type <code>SymTagExe</code>. " \
+    "A page with the extended information of that symbol will also appear in the center of the window.</p>" \
+    "<p>Double-click a debug symbol in the tree to open a page with the extended info of that symbol.</p>" \
+    "<p>Expand a debug symbol in the tree to see it's tree children, which appear in the following order:</p>" \
+    "<ol>" \
+    "<li>attribute symbols</li>" \
+    "<li><code>Find children...</code> command</li>" \
+    "</ol>" \
+    "<p>Double-click the <code>Find children...</code> command to search for child debug symbols. " \
+    "If a search hasn't been performed yet, expanding the command does the same. " \
+    "Symbols found this way appear as tree children of the command.</p>" \
+    "<a name='feedback' /><h4>Feedback</h4>" \
+    "<p>Please report any bugs or requests of improvement to:</p>" \
     "<ul><li>https://github.com/flaviojs/pydia</li></ul>" \
     "</body></html>"
 
